c1c648d2dd02ae97f6ed30c29a1497d933742f3c
hanganflorin <hanganflorin09@gmail.com>
2018-10-31 02:53:18 +0200

    Finished Lab1 LFTC

--- lftc/lab1/gcc.py
+++ lftc/lab1/gcc.py
@@ -1,27 +1,30 @@
 from os import sys, path
 from random import randint
+from sortedcontainers import SortedDict
 
 
 class Scanner:
     def __init__(self, fileName):
-        # .cpp filename
+        #.cpp filename
         self.fileName = fileName
         # file to store the program internal form
-        self.pifFileName = path.splitext(path.basename(fileName))[0] + "_pif.txt"
+        self.pifFileName = "output/" + path.splitext(path.basename(fileName))[0] + "_pif.txt"
         # clear file if already exist
         open(self.pifFileName, 'w').close()
         # file to store the identifiers table
-        self.outputIdentifiersTable = path.splitext(path.basename(fileName))[0] + "_id_table.txt"
+        self.outputIdentifiersTable = "output/" + path.splitext(path.basename(fileName))[0] + "_id_table.txt"
         # file to store the constants table
-        self.outputConstantsTable = path.splitext(path.basename(fileName))[0] + "_const_table.txt"
+        self.outputConstantsTable = "output/" + path.splitext(path.basename(fileName))[0] + "_const_table.txt"
         # dictionary for all the program symbols (if, for, while, else, int, float, etc)
         self.codificationTable = {}
         # dictionary for storing the identifiers, as a pair identifier -> integer id
-        self.identifiersTable = {}  # TODO LISTA ORDONATA
+        self.identifiersTable = SortedDict()
         # dictionary for storing the identifiers, as a pair constant -> integer id
-        self.constantsTable = {}  # TODO LISTA ORDONATA
+        self.constantsTable = SortedDict()
         # load all the toy language symbols
         self.populateCodificationTable()
+        self.inputText = self.getInputText()
+        self.currentIndex = 0
 
     # method loads symbol table in memory from the disc
     def populateCodificationTable(self):
@@ -39,181 +42,156 @@ class Scanner:
             print("ERROR: Symbols file not found!")
             sys.exit()
 
-    # # method returns a random integer that is not in the values array
-    # def randomNotIn(self, values):
-    #   # returns a random vaue between 1 and 100000
-    #   r = randint(1, 100000)
-    #   # while that value already exists in the array values
-    #   while r in values:
-    #     # generate a new one
-    #     r = randint(1, 100000)
-    #   # return the number generated
-    #   return r
+    #get all text from filename
+    def getInputText(self):
+        try:
+            # open the file for reading
+            f = open(self.fileName, "r")
+            return f.read() + "\n"
+        # if file was not found, print error and fail fast
+        except IOError:
+            print("ERROR: Source file not found!")
+
+    #get next character from input.txt. Throws error if no more characters
+    def getNextCharacter(self):
+        if self.currentIndex >= len(self.inputText):
+            raise EOFError
+        c = self.inputText[self.currentIndex]
+        self.currentIndex += 1
+        return c
+
+    def pickNextCharacter(self):
+        if self.currentIndex >= len(self.inputText):
+            return ''
+        return self.inputText[self.currentIndex]
 
     # method append buff to the file outputFileName
-    def appendToPifFile(self, buff):
+    def appendToPifFile(self, buff, token = ''):
+        #for debugging
+        buff = buff.rstrip('\n') + " " + token + "\n"
         # open file
         with open(self.pifFileName, "a") as f:
             # write the string buff as a new line
             f.write(buff)
 
     # method write the identifier and constant tables
-    def writeTables(self):  # TODO HABAR NU AM
+    def writeTables(self):
         # open file for identifiers table
         with open(self.outputIdentifiersTable, "w") as f:
             # iterate through the identifiers table
-            for (key, val) in self.identifiersTable.iteritems():
+            for (key, val) in self.identifiersTable.items():
                 # write the pair on a new line
                 f.write("%s %s\n" % (key, val))
         # open file for constant table
         with open(self.outputConstantsTable, "w") as f:
             # iterate through the constants table
-            for (key, val) in self.constantsTable.iteritems():
+            for (key, val) in self.constantsTable.items():
                 # write the pair on a new line
                 f.write("%s %s\n" % (key, val))
 
     # method decides if _token is a symbol or an identifier
     def addToken(self, _token):
-        # if the token is in the symbols table, then it's a symbol
+        # if the token is in the codification table, then it's a symbol
         if _token in self.codificationTable:
-            return self.addSymbol(_token)
+            self.appendToPifFile(str(self.codificationTable[_token]) + " -1\n", _token)
         # else, it must be an identifier
         else:
-            return self.addIdentifier(_token)
+            self.addIdentifier(_token)
 
-    # method prints the symbol to the program internal form file output
     def addSymbol(self, _symbol):
         # if the symbol is in the symbol table
         if _symbol in self.codificationTable:
             # print it
-            self.appendToPifFile(str(self.codificationTable[_symbol]) + " 0\n")
+            self.appendToPifFile(str(self.codificationTable[_symbol]) + " -1\n", _symbol)
             return True
         else:
-            # return false because _symbol is not a valid symbol, and then throw an error
+            # return false because _symbol is not a valid symbol
             return False
 
     # method prints identifier and it's id to the output file
     def addIdentifier(self, _id):
-        # assign a new, unused integer id for the current identifier
         if _id not in self.identifiersTable:
-            self.identifiersTable[_id] = self.randomNotIn(self.identifiersTable.values())
+            self.identifiersTable[_id] = len(self.identifiersTable) + 1
         # print to program internal form output file
         self.appendToPifFile(
-            self.codificationTable["identifier"] + " " + str(self.identifiersTable[_id]) + "\n")
-        return True
+            self.codificationTable["identifier"] + " " + str(self.identifiersTable[_id]) + "\n", _id)
 
     # method adds a constant to the table and prints it to the output file
     def addConstant(self, _val):
         # assign a new, unsued integer id for the current identifier
         if _val not in self.constantsTable:
-            self.constantsTable[_val] = self.randomNotIn(self.constantsTable.values())
-        # print to the program internl form output file
+            self.constantsTable[_val] = len(self.constantsTable) + 1
+        # print to the program internal form output file
         self.appendToPifFile(
-            self.codificationTable["constant"] + " " + str(self.constantsTable[_val]) + "\n")
-        return True
-
-    # generator method returns all the characters line by line
-    def getNextChar(self):
-        try:
-            # open the file for reading
-            f = open(self.fileName, "r")
-            # read all the lines
-            lines = f.readlines()
-            # iterate through lines
-            for lineIndex, line in enumerate(lines):
-                # iterate through columns
-                for columnIndex, ch in enumerate(line):
-                    # yield the line, column index and the current character
-                    yield [lineIndex, columnIndex, ch]
-        # if file was not found, print error and fail fast
-        except IOError:
-            print("ERROR: Source file not found!")
-            sys.exit()
+            self.codificationTable["constant"] + " " + str(self.constantsTable[_val]) + "\n", _val)
 
     # method tokenize the source file
     def tokenize(self):
-        # get the generator
-        charIterator = self.getNextChar()
         try:
-            # get the next values from the generator
-            (i, j, ch) = next(charIterator)
-            # we iterate while we get a StopIteration exception
+            ch = self.getNextCharacter()
+            #iterate character by character
             while True:
                 # in case we have an alphabet character (a, b, .. z, A, B, .. Z)
                 if ch.isalpha():
                     # variable to store the current identifier
                     _id = ""
-                    # we iterate with the iterator while we have valid identifier characters
+                    # we iterate while we have valid identifier characters
                     while ch.isalpha() or ch == '_':
                         # append the current character to _id
                         _id += ch
                         # get the next character
-                        (i, j, ch) = next(charIterator)
+                        ch = self.getNextCharacter()
                     # at the end, if the lenght of the iterator is more than the max allowed lenght
                     # throw an error, and fail fast
                     if len(_id) > 250:
-                        print("ERROR: Identifier has too many characters. (line, col) = (%d, %d)" % (i, j))
+                        print("ERROR: Identifier has too many characters.")
                         sys.exit()
-                    # add the token to the interal hashmaps
+                    # add the token
                     self.addToken(_id)
-                    _id = ""
                 # in case we have a digit (0-9)
-                elif ch.isdigit():
+                elif ch.isdigit() or (ch in "+-" and self.pickNextCharacter().isdigit()):
                     # variable stores the current constant
-                    _val = ""
+                    _val = ch
+                    ch = self.getNextCharacter()
                     # while there is a digit or if the current character is .
                     while ch.isdigit() or ch == '.':
                         # append the character to the constant
                         _val += ch
                         # get next character
-                        (i, j, ch) = next(charIterator)
+                        ch = self.getNextCharacter()
                     # add the constant to the program internal form and to the internal hashmaps
                     self.addConstant(_val)
                 # ignore whitespace characters
                 elif ch.isspace():
                     # get the next character
-                    (i, j, ch) = next(charIterator)
+                    ch = self.getNextCharacter()
                 # else, we may have a symbol or an invalid identifier
                 else:
-                    # get the first character and store it in the _id variable
-                    _id = ch
-                    # try to get the second one for cases like >=, <=, == and !=
-                    try:
-                        # store last character
-                        last = ch
-                        # get the next cahracter
-                        (i, j, ch) = next(charIterator)
-                        # if we are in one of the cases >=, <=, == or !=, we update the variable
-                        if (last == '>' or last == '<' or last == '=' or last == '!') and ch == '=':
-                            _id = _id + ch
-                    except StopIteration:
-                        # no other character left to get, we simply pass cause we may have }
-                        pass
+                    # pick the next character
+                    next = self.pickNextCharacter()
+                    # if we are in one of the cases >=, <=, == or !=, we update the variable
+                    if ch+next in ["<=", ">=", "!=", "=="]:
+                        _id = _id + next
                     # if we couldn't add the symobl, we throw an error because it is an unexpected
                     # symbol identifier
-                    if not self.addSymbol(_id):
-                        print("ERROR: Syntax Error detected at (line, col) = (%d, %d)" % (i, j))
-                        print("ERROR: Unexpected token '%s'" % _id)
+                    if not self.addSymbol(ch):
+                        print("ERROR: Unexpected token '%s'" % ch)
                         sys.exit()
+                    ch = self.getNextCharacter()
+
         # in case we reached the end of the iteration
-        except StopIteration:
+        except EOFError:
             self.writeTables()
             print("> finish")
             return
 
 
-# method scans and tokenize the filename source code
-def scan(filename):
-    # create the scaner
-    s = Scanner(filename)
-    # call the tokenize method
-    s.tokenize()
-
-
 # if name is main
 if __name__ == '__main__':
-    # get the first argument of the args
-    # log it
-    print("> scanning " + str(sys.argv[1]) + "...")
-    # scan that filename
-    scan(sys.argv[1])
+    # filename = str(sys.argv[1])
+    filename = "input/circle.cpp"
+    # filename = "input/test.cpp"
+
+    print("> scanning " + filename + "...")
+    s = Scanner(filename)
+    s.tokenize()
---
+++ lftc/lab1/input/test.cpp
@@ -0,0 +1,10 @@
+int main()
+{
+  int b;
+  int c;
+  int a;
+  a = -10.23;
+  b = 0.222;
+  c = a + b;
+  return 0;
+}
